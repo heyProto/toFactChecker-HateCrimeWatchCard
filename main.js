7900cf409b4186d52da978a743904304ac63eba6
kedar khedekar <kd1003v@gmail.com>
2018-06-06 19:53:37 +0530

    performance changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Card from './src/js/card.jsx';
 
 window.ProtoGraph = window.ProtoGraph || {};
@@ -32,14 +32,17 @@ ProtoGraph.Card.toIndiaSpendCard.prototype.renderScreenshot = function(data) {
 }
 
 ProtoGraph.Card.toIndiaSpendCard.prototype.render = function() {
-   ReactDOM.render(
-    <Card
-      dataURL={this.options.data_url}
-      selector={this.options.selector}
-      clickCallback={this.options.onClickCallback}
-      mode={this.mode}
-      ref={(e) => {
-        this.containerInstance = this.containerInstance || e;
-      }} />,
-    this.options.selector);
+    render( <
+        Card dataURL = { this.options.data_url }
+        selector = { this.options.selector }
+        clickCallback = { this.options.onClickCallback }
+        siteConfigURL = { this.options.site_config_url }
+        mode = { this.mode }
+        ref = {
+            (e) => {
+                this.containerInstance = this.containerInstance || e;
+            }
+        }
+        />,
+        this.options.selector);
 }
